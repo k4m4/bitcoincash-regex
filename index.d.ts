6b7ed46c464540321846c6f4c2a20d13b73d3a91
k4m4 <nikolaskam@gmail.com>
2019-12-29 09:20:54 -0500

    Fix inconsistent indentation

--- index.d.ts
+++ index.d.ts
@@ -5,35 +5,35 @@ declare namespace bch {
 		@default false
 		*/
 		readonly exact?: boolean;
-    }
-    /**
-    Available BCH formats.
-    */
-    type Format = 'legacy' | 'cashaddr';
+	}
+	/**
+	Available BCH formats.
+	*/
+	type Format = 'legacy' | 'cashaddr';
 }
 
 declare const bch: {
-    /**
+	/**
 	Returns a regex for matching BCH addresses.
 	@example
 	```
 	import bch = require('bitcoincash-regex')
 	bch().test('nodejsrocks 19hZx234vNtLazfx5J2bxHsiWEmeYE8a7k')
-    //=> true
+	//=> true
 	```
 	*/
-    (options?: bch.Options): RegExp;
+	(options?: bch.Options): RegExp;
 
-    /**
+	/**
 	Returns a regex for matching specific BCH format addresses.
 	@example
 	```
 	import bch = require('bitcoincash-regex')
 	bch.format('cashaddr', {exact: true}).test('bitcoincash:qpm2qsznhks23z7629mms6s4cwef74vcwvy22gdx6a')
-    //=> true
+	//=> true
 	```
 	*/
-    format(format: bch.Format, options?: bch.Options): RegExp;
+	format(format: bch.Format, options?: bch.Options): RegExp;
 }
 
 export = bch;
